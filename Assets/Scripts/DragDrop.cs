d67553fe25dad55a279b98367b43175892cf76ad
FranPonce3D <117677911+FranPonce3D@users.noreply.github.com>
2024-03-05 15:00:45 +0100

    Limit interactable area

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -6,6 +6,9 @@ public class DragDrop : MonoBehaviour
 {
     Vector3 mousePositionOffset;
 
+    bool limitContact;
+    
+
     private Vector3 GetMouseWorldPossition()
     {
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -14,10 +17,30 @@ public class DragDrop : MonoBehaviour
     private void OnMouseDown()
     {
         mousePositionOffset = gameObject.transform.position - GetMouseWorldPossition();
+        
+    }
+
+    private void OnMouseUp()
+    {
+        limitContact = false;
     }
 
     private void OnMouseDrag()
     {
-        transform.position = GetMouseWorldPossition() + mousePositionOffset;
+        if (limitContact == false)
+        {
+            transform.position = GetMouseWorldPossition() + mousePositionOffset;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Limit")
+        {
+            limitContact = true;
+            //moveObject = collision.gameObject.GetComponent<InteractuableLimit>().relocate;
+            //transform.position = transform.position + moveObject;
+        }
     }
 }
