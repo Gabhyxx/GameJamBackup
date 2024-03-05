2a18e5372d7a722bda2517bce1dcfc6027ead01a
Gabhyxx <jgabrielpg00@gmail.com>
2024-03-05 15:02:36 +0100

    Name Generator 1

---
+++ Assets/Scripts/CharNameGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharNameGenerator : MonoBehaviour
+{
+    List<string> firstNames = new List<string>
+    {
+
+    };
+
+    List<string> lastNames = new List<string> 
+    {
+        
+    };
+
+    List<string> crime = new List<string>
+    {
+
+    };
+
+    List<string> description1 = new List<string>
+    {
+
+    };
+
+    List<string> description2 = new List<string>
+    {
+
+    };
+
+    List<string> description3 = new List<string>
+    {
+
+    };
+}
